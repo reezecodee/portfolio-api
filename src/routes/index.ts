e6335b589343b7b18e8d85b1826a527ff9cef611
Reeze <azfaalharits25@gmail.com>
2025-06-14 14:21:46 +0700

    feat: CORS

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import skillRoute from "./skills";
 import projectRoute from "./projects";
 import blogRoute from "./blogs";
 import cryptoRoute from "./cryptos";
 
 const app: Hono = new Hono();
+app.use("*", cors());
 
 app.route("/skills", skillRoute);
 app.route("/cryptos", cryptoRoute);
