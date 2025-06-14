17f89d265307e9f3f19bcac33ebca0f0045dc2b3
Reeze <azfaalharits25@gmail.com>
2025-06-14 14:47:14 +0700

    feat: grouped project api

--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -6,7 +6,17 @@ const projectRoute: Hono = new Hono();
 projectRoute.get("/", async (c) => {
   const { data, error } = await supabase.from("projects").select("*");
   if (error) return c.json({ error: error.message }, 500);
-  return c.json(data);
+
+  const grouped = data.reduce((acc, item) => {
+    if (!acc[item.project_type]) {
+      acc[item.project_type] = [];
+    }
+
+    acc[item.project_type].push.item;
+    return acc;
+  }, {});
+
+  return c.json(grouped);
 });
 
 export default projectRoute;
